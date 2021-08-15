c738a0272c8cf896ef1a41b965d0fe94c8ef31f4
Nathanael Ferry P <ntnferry1505@gmail.com>
2021-08-15 19:58:05 +0700

    change url

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,10 @@ import { AppWrapper } from '../contexts/state';
 import { AuthWrapper } from '../contexts/AuthContext';
 
 import '../styles/flatly.css';
-// import 'sweetalert2/src/sweetalert2.scss';
 
 function MyApp({ Component, pageProps }) {
+  console.log(process.env.STAGE);
+
   return (
     <>
       <AppWrapper>
--- utils/const.js
+++ utils/const.js
@@ -1,7 +1,8 @@
 if (process.env.STAGE === 'PROD') {
   var API_URL = 'https://veon-tools-backend.herokuapp.com/api';
 } else {
-  var API_URL = 'http://localhost:3001/api';
+  // var API_URL = 'http://localhost:3001/api';
+  var API_URL = 'https://veon-tools-backend.herokuapp.com/api';
 }
 
 export { API_URL };
